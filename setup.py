3494894910363e9941a47f45d8a88c073842017a
Sofyan Sugianto <sofyanartem@gmail.com>
2023-08-14 01:23:51 +0700

    wip - add setup.py file

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+  
+setup(
+    name='vmcreator',
+    version='0.1',
+    description='A simple cli VM creator using libvirt and yaml',
+    author='artemtech',
+    author_email='sofyan@artemtech.id',
+    packages=['vmcreator'],
+    install_requires=[
+        'requests',
+        'PyYAML',
+        'lxml',
+        'libvirt-python'
+    ],
+)
